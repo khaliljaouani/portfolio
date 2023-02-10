8eaa274d81d2a2cd4c943c1f9270b4410b285577
khaliljaouani <khalil.jaouani29@gmail.com>
2023-02-10 17:49:06 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./App.css"
+import Header from './components/header/Header';
+const app = () => {
+  return (
+    <>
+    <Header />
+    
+    </>
+  )
+}
+export default app
