218aa5678cd6dff8450dfaccfc53181b1d7d8dec
khaliljaouani <khalil.jaouani29@gmail.com>
2023-03-08 03:41:06 +0100

    Skills Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css"
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
+import Skills from './components/skills/Skills';
 
 const app = () => {
   return (
@@ -12,6 +13,7 @@ const app = () => {
     <main className='main'>
       <Home />
       <About />
+      <Skills />
     </main>
     </>
   )
